da685c18fa5401fe84d85b00bf169ff7db70bccc
Patrick Nilan <nilan.patrick@gmail.com>
2023-06-03 11:42:33 -0700

    Passes all LiveServerIntegrationSpec tests

--- server/request-handler.js
+++ server/request-handler.js
@@ -12,52 +12,28 @@ this file and include it in basic-server.js so that it actually works.
 
 **************************************************************/
 var { Buffer } = require('node:buffer');
-let storage = [];
+var storage = [];
 var requestHandler = function (request, response) {
-  // const { method, url } = request;
-  // var body = [];
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
-
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
-
   var headers = defaultCorsHeaders;
   headers['Content-Type'] = 'application/json';
-  var storage = [];
 
+  // GET GET GET GET
   if (request.url === '/classes/messages' && request.method === 'GET') {
-    // let body = [];
-    // body = JSON.stringify(body);
-    storage = JSON.stringify(storage);
-
+    request.on('data', (chunk) => {
+      storage.push(chunk);
+    }).on('end', () => {
+    });
     var statusCode = 200;
     response.writeHead(statusCode, headers);
-    response.end(storage);
-    // });
+    response.end(JSON.stringify(storage));
+    // POST POST POST POST POST
   } else if (request.url === '/classes/messages' && request.method === 'POST') {
     let body = [];
     request.on('data', (chunk) => {
-      console.log(chunk);
-      console.log(chunk.toString());
       body.push(chunk);
     }).on('end', () => {
-      // body = JSON.stringify(body); //passes test
-      // body = Buffer.concat(body).toString(); //option 2: does not pass test
-      Buffer.alloc(0);
-      body = JSON.stringify(Buffer.concat(body)); //option 3: does not pass test but will return any objects we pass in
-      // response.statusCode = 201;
-      storage.push(body);
-      console.log(body, storage);
+      body = body.toString();
+      storage.push(JSON.parse(body));
       statusCode = 201;
       response.writeHead(statusCode, headers);
       response.end(body);
@@ -65,18 +41,28 @@ var requestHandler = function (request, response) {
 
   } else {
     statusCode = 404;
-    // response.statusCode = 404;
     response.writeHead(statusCode, headers);
     response.end();
   }
-  // console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  // var statusCode = 200;
-  // headers['Content-Type'] = 'application/json';
-  // // response.statusCode = statusCode;
-  // response.writeHead(statusCode, headers);
-  // response.end('Hello, world');
-
 };
+
+
+// const { method, url } = request;
+// var body = [];
+// Request and Response come from node's http module.
+//
+// They include information about both the incoming request, such as
+// headers and URL, and about the outgoing response, such as its status
+// and content.
+//
+// Documentation for both request and response can be found in the HTTP section at
+// http://nodejs.org/documentation/api/
+
+// Do some basic logging.
+//
+// Adding more logging to your server can be an easy way to get passive
+// debugging help, but you should always be careful about leaving stray
+// console.logs in your code.
 
 // The outgoing status.
 
@@ -118,4 +104,4 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
-exports.handleRequest = requestHandler;
+exports.requestHandler = requestHandler;
